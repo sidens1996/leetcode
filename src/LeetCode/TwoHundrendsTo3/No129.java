f66d8d12245040001ceb5d63c752945964df7779
sidens1996 <1424285844@qq.com>
2020-01-07 22:14:25 +0800

    add 129

--- src/LeetCode/TwoHundrendsTo3/No129.java
+++ src/LeetCode/TwoHundrendsTo3/No129.java
@@ -1,5 +1,7 @@
 package LeetCode.TwoHundrendsTo3;
 
+import DataStructure.TreeNode;
+
 /**
  * @ClassName: No129
  * @Description: leetcode第129题，Sum Root to Leaf Numbers
@@ -9,4 +11,23 @@ package LeetCode.TwoHundrendsTo3;
  **/
 
 public class No129 {
+
+    int sum = 0;
+
+    public int sumNumbers(TreeNode root) {
+        sum(0, root);
+        return sum;
+    }
+
+    public void sum(int val, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        int k = (val * 10 + root.val);
+        if (root.left == null && root.right == null) {
+            sum += k;
+        }
+        sum(k, root.left);
+        sum(k, root.right);
+    }
 }
